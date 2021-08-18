eb61db63f7ad62a3ebc84768986e55f5c1021c6e
itgitnit <Thakurnikhil2210@gmail.com>
2021-08-18 16:42:31 -0700

    card added

--- src/Step5.js
+++ src/Step5.js
@@ -1,28 +1,68 @@
 import React from "react";
-import "./select.css"
+import "./step5.css"
 import { NavLink } from "react-router-dom";
 
 const Step5 = () => {
     return (
         <>
-            <div className="container">
-                <div className="title">What you want to do now??</div>
-                <form action="#">
-                    <a href="">
-                        <div className="button">
-                            <NavLink to="./addcourses"><input type="submit" value="Account Customization & Company Details" /></NavLink>
+            <div className="heading">
+                <h1>What Do You Want Do Now??</h1>
+            </div>
+            <div className="containerAll">
+                <div className="cardContainer">
+                    <div className="card">
+                        <div className="circle">
+                            <h2>01</h2>
                         </div>
-                    </a>
-                    <div className="button">
-                        <NavLink to="./addcourses"><input type="submit" value="How LMS Works??" /></NavLink>
+                        <div className="content">
+                            <div className="para">
+                                <h2>Account Customization & Company Details</h2>
+                            </div>
+                            <div className="button">
+                                <NavLink to="./addcourses" className="a">Click Here</NavLink>
+                            </div>
+                        </div>
                     </div>
-                    <div className="button">
-                        <NavLink to="./addcourses"><input type="submit" value="Start Making Your Courses" /></NavLink>
+                    <div className="card">
+                        <div className="circle">
+                            <h2>02</h2>
+                        </div>
+                        <div className="content">
+                            <div className="para">
+                                <h2>How LMS Works?</h2>
+                            </div>
+                            <div className="button">
+                                <NavLink to="./addcourses" className="a">Click Here</NavLink>
+                            </div>
+                        </div>
                     </div>
-                    <div className="button">
-                        <NavLink to="./addcourses"> <input type="submit" value="Go to Administration Panel" /></NavLink>
+                    <div className="card">
+                        <div className="circle">
+                            <h2>03</h2>
+                        </div>
+                        <div className="content">
+                            <div className="para">
+                                <h2>Start Making Your Courses</h2>
+                            </div>
+                            <div className="button">
+                                <NavLink to="./addcourses" className="a">Click Here</NavLink>
+                            </div>
+                        </div>
                     </div>
-                </form>
+                    <div className="card">
+                        <div className="circle">
+                            <h2>04</h2>
+                        </div>
+                        <div className="content">
+                            <div className="para">
+                                <h2>Go To Administration Panel</h2>
+                            </div>
+                            <div className="button">
+                                <NavLink to="./addcourses" className="a">Click Here</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
